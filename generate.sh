d73ef8268ecafde5ad8aec2bb916b221eabaef70
Karl Benedict <kbene@unm.edu>
2019-08-18 18:00:39 -0600

    updated with new Pandoc syntax and tex file generation

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-/usr/local/bin/pandoc --standalone --toc --latex-engine=pdflatex  -V geometry:margin=1in -V fontsize:11pt -o OILS515_syllabus.pdf OILS515_syllabus.md
+/usr/local/bin/pandoc --standalone --template=custom.latex --pdf-engine=pdflatex  -V geometry:margin=1in -V fontsize:11pt -o OILS515_syllabus.pdf OILS515_syllabus.md
 /usr/local/bin/pandoc --toc -s --standalone --css=page.css -o OILS515_syllabus.html OILS515_syllabus.md
 /usr/local/bin/pandoc --toc -s --standalone --css=page.css -o index.html OILS515_syllabus.md
 /usr/local/bin/pandoc -s -o OILS515_syllabus.epub OILS515_syllabus.md
+/usr/local/bin/pandoc -s -o OILS515_syllabus.tex OILS515_syllabus.md
 
