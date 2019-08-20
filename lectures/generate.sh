c40051e9f3aa369d9e10b49f17979272cec25cf3
Karl Benedict <kbene@unm.edu>
2019-08-20 15:52:55 -0600

    updated class overview slide deck

--- lectures/generate.sh
+++ lectures/generate.sh
@@ -4,5 +4,5 @@ find . -iname "*.md" -exec /usr/local/bin/pandoc --slide-level 3 -c slides.css -
 find . -iname "*.md" -exec /usr/local/bin/pandoc --slide-level 3 -c slides.css -c slidesSpecific.css --standalone -t dzslides \{\} -o \{\}.slides.linked.html \;
 find . -iname "*.md" -exec /usr/local/bin/pandoc --toc -c ../page.css --self-contained --standalone \{\} -o \{\}.html \;
 find . -iname "*.md" -exec /usr/local/bin/pandoc --toc -c ../page.css --standalone \{\} -o \{\}.linked.html \;
-find . -iname "*.md" -exec /usr/local/bin/pandoc --standalone --toc --latex-engine=pdflatex  -V geometry:margin=1in -V fontsize:11pt  \{\} -o \{\}.pdf \;
+find . -iname "*.md" -exec /usr/local/bin/pandoc --standalone --toc --pdf-engine=pdflatex  -V geometry:margin=1in -V fontsize:11pt  \{\} -o \{\}.pdf \;
 
